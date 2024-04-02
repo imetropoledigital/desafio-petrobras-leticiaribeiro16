2ac2fb2f0ffd5848897ec82101804387b4c5395f
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2024-04-01 23:42:23 -0300

    Update integrate.js

--- integrate.js
+++ integrate.js
@@ -3,8 +3,15 @@
  * @param {Array} data Array de objetos que contém par (x,y) 
  * @returns valor total da integral numérica
  */
+
 function integrate(data){
-    return -1
- }
+    let total = 0;
+    for(let i = 0; i < data.length - 1; i++) {
+        let dx = data[i+1].x - data[i].x;
+        let avgHeight = (data[i].y + data[i+1].y) / 2;
+        total += dx * avgHeight;
+    }
+    return total;
+}
  
  module.exports = integrate
